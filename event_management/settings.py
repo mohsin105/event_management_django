f1211be98641a6693201283018732be4c1316750
Mohsin <mohsinibnaftab@gmail.com>
2025-02-17 17:54:17 +0600

    error solve attempt

--- event_management/settings.py
+++ event_management/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-=8x(82y5tb1&crvdk97^$grq+wm@8yj09%xaq_+b(0=kub#tyy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

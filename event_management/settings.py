2f30ea402e5842335b90810fae91726d51d0c32a
Mohsin <mohsinibnaftab@gmail.com>
2025-09-16 00:57:17 +0600

    render db integrated

--- event_management/settings.py
+++ event_management/settings.py
@@ -89,25 +89,25 @@ WSGI_APPLICATION = 'event_management.wsgi.application'
 # }
 
 # for PostgresQl
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'event_management',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'event_management',
+#         'USER': 'postgres',
+#         'PASSWORD': '123456',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
 
 # for online DB on render.com
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         # Replace this value with your local database's connection string.
-#         default='postgresql://event_management_django_db_user:Sq5qC2g3LAUo5PG110MjfhMEKijeOu2w@dpg-cuhmiv1u0jms73ac5jb0-a.oregon-postgres.render.com/event_management_django_db',
-#         conn_max_age=600
-#     )
-# }
+DATABASES = {
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://event_management_django_db_3wpb_user:wXqRWx3SLiXkouJgbexdN6DJbmbYU3K2@dpg-d346033uibrs73b4pvc0-a.oregon-postgres.render.com/event_management_django_db_3wpb',
+        conn_max_age=600
+    )
+}
 
 
 # Password validation
